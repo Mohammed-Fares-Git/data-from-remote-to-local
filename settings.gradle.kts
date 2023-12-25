10e0f7ca03bf0994b46e206ff9a1a7df1f94f09c
Mohammed-Fares-Dev <mohammedfarestech@gmail.com>
2023-12-25 17:03:49 +0100

    start a frech project with necessary dependencies

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mohammed_Fares_C1_Interfaces_Grafiques"
+include(":app")
+ 
